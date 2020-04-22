02f3e05917daeb8e33a9acbc79249df7e1732057
kp1129 <katya.p.1129@gmail.com>
2020-04-21 17:04:32 -0700

    add /login endpoint, registered users can log in

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -28,9 +28,43 @@ router.post("/register", (req, res) => {
 })
 
 // login
+router.post("/login", (req, res) => {
+    let { email, password } = req.body;
 
+    // first find them by email
+    Users.findBy({ email })
+    .then(([user]) => {
+        // if user exists, check that the passwords match
+        if(user && bcrypt.compareSync(password, user.password)){
+            // proceed to log them in
+            // issue a token
+            const token = generateToken(user);
+            // send token to the client
+            res.status(200).json({ message: "Welcome!", token })
+        } else {
+            res.status(401).json({ message: "Credentials could not be authenticated"})
+        }
+    })
+    .catch(error => res.status(500).json({ message: "Oops! Something went wrong" }))
+})
 
 
 // logout
+
+
+
+
+function generateToken(user) {
+    // jwt.sign() expects payload, secret, and options
+    const payload = {
+        userId: user.id
+    };
+    const secret = secrets.jwtSecret;
+    const options = {
+        expiresIn: "1d"
+    }
+
+    return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
